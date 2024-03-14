df8eaf5bbe71f04969306b1995f4cacc3b2f64bb
Stéphane <stephane@MacBook-Pro-de-Stephane.local>
2024-03-14 10:10:39 +0100

    debug

--- services/controllerWrapper.js
+++ services/controllerWrapper.js
@@ -1,12 +1,12 @@
-const client = require('.././models/dbClient');
+function controllerWrapper(mdwFunction) {
+  return async function(req, res, next) {
+    try {
+      await mdwFunction(req, res, next);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Unexpected server error. Please try again later." });
+    }
+  };
+}
 
-const loadSessionUserInLocals = async (req, res, next) => {
-  if (req.session.user) {
-    res.locals.user = req.session.user;
-    // req.user = useer;
-  }
-
-  next();
-};
-
-module.exports = loadSessionUserInLocals;
+module.exports = controllerWrapper;
